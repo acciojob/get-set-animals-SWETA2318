7f8050e42e8559ed2fb2b5b70a066a6d4c0d51ba
SWETA2318 <150820032+SWETA2318@users.noreply.github.com>
2024-01-11 18:27:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,47 @@
-//complete this code
-class Animal {}
+// Animal class
+class Animal {
+    constructor(species) {
+        this._species = species;
+    }
 
-class Dog extends Animal {}
+    // Getter for species
+    get species() {
+        return this._species;
+    }
 
-class Cat extends Animal {}
+    // Method to make a sound
+    makeSound() {
+        console.log("Generic animal sound");
+    }
+}
 
-// Do not change the code below this line
-window.Animal = Animal;
-window.Dog = Dog;
-window.Cat = Cat;
+// Cat class extending Animal
+class Cat extends Animal {
+    // Method specific to Cat
+    purr() {
+        console.log("purr");
+    }
+}
+
+// Dog class extending Animal
+class Dog extends Animal {
+    // Method specific to Dog
+    bark() {
+        console.log("woof");
+    }
+}
+
+// Example usage
+const genericAnimal = new Animal("Generic Species");
+console.log(genericAnimal.species); // Output: Generic Species
+genericAnimal.makeSound(); // Output: Generic animal sound
+
+const cat = new Cat("Domestic Cat");
+console.log(cat.species); // Output: Domestic Cat
+cat.makeSound(); // Output: Generic animal sound
+cat.purr(); // Output: purr
+
+const dog = new Dog("Golden Retriever");
+console.log(dog.species); // Output: Golden Retriever
+dog.makeSound(); // Output: Generic animal sound
+dog.bark(); // Output: woof
